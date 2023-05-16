a6eaf65e6cd7fe2f17ede3594b0baef9236e345e
MATHEUS FRANCISCO FERREIRA DOS SANTOS <matheus111111111111@gmail.com>
2023-05-16 14:51:22 -0300

    mudanças

--- scripts/mostrarElementos.js
+++ scripts/mostrarElementos.js
@@ -1,54 +1,35 @@
 function mostrarCamera() {
-    var cameraDiv = document.getElementById('camera');
-    cameraDiv.style.display = 'block';
+  var cameraDiv = document.getElementById('interactive');
+  var codigoInternoInput = document.getElementById('codigoInternoInput');
+  var codigoInternoButton = document.getElementById('codigoInternoButton');
+  var nomeProdutoInput = document.getElementById('nomeProdutoInput');
+  var nomeProdutoButton = document.getElementById('nomeProdutoButton');
+  
+  cameraDiv.style.display = 'block';
+  codigoInternoInput.style.display = 'none';
+  codigoInternoButton.style.display = 'none';
+  nomeProdutoInput.style.display = 'none';
+  nomeProdutoButton.style.display = 'none';
 }
-function mostrarInputs(elementId) {
-    if (elementId === 'codigoInterno') {
-      var codigoInternoInput = document.getElementById('codigoInternoInput');
-      var codigoInternoButton = document.getElementById('codigoInternoButton');
-      
-      codigoInternoInput.style.display = 'block';
-      codigoInternoButton.style.display = 'block';
-    } else if (elementId === 'nomeProduto') {
-      var nomeProdutoInput = document.getElementById('nomeProdutoInput');
-      var nomeProdutoButton = document.getElementById('nomeProdutoButton');
-      
-      nomeProdutoInput.style.display = 'block';
-      nomeProdutoButton.style.display = 'block';
-    }
-  }
-  function mostrarCamera() {
-    var cameraDiv = document.getElementById('camera');
-    var codigoInternoInput = document.getElementById('codigoInternoInput');
-    var codigoInternoButton = document.getElementById('codigoInternoButton');
-    var nomeProdutoInput = document.getElementById('nomeProdutoInput');
-    var nomeProdutoButton = document.getElementById('nomeProdutoButton');
-    
-    cameraDiv.style.display = 'block';
-    codigoInternoInput.style.display = 'none';
-    codigoInternoButton.style.display = 'none';
-    nomeProdutoInput.style.display = 'none';
-    nomeProdutoButton.style.display = 'none';
-  }
 
-  function mostrarInputs(elementId) {
-    var cameraDiv = document.getElementById('camera');
-    var codigoInternoInput = document.getElementById('codigoInternoInput');
-    var codigoInternoButton = document.getElementById('codigoInternoButton');
-    var nomeProdutoInput = document.getElementById('nomeProdutoInput');
-    var nomeProdutoButton = document.getElementById('nomeProdutoButton');
-    
-    cameraDiv.style.display = 'none';
-    
-    if (elementId === 'codigoInterno') {
+function mostrarInputs(elementId) {
+  var cameraDiv = document.getElementById('interactive');
+  var codigoInternoInput = document.getElementById('codigoInternoInput');
+  var codigoInternoButton = document.getElementById('codigoInternoButton');
+  var nomeProdutoInput = document.getElementById('nomeProdutoInput');
+  var nomeProdutoButton = document.getElementById('nomeProdutoButton');
+  
+  cameraDiv.style.display = 'none';
+  
+  if (elementId === 'codigoInterno') {
       codigoInternoInput.style.display = 'block';
       codigoInternoButton.style.display = 'block';
       nomeProdutoInput.style.display = 'none';
       nomeProdutoButton.style.display = 'none';
-    } else if (elementId === 'nomeProduto') {
+  } else if (elementId === 'nomeProduto') {
       codigoInternoInput.style.display = 'none';
       codigoInternoButton.style.display = 'none';
       nomeProdutoInput.style.display = 'block';
       nomeProdutoButton.style.display = 'block';
-    }
   }
+}
